4fabd0f328ccb044122c4e00770516376182a28b
manirabona-programer <hseal419@gmail.com>
2023-06-06 09:13:40 +0200

    chore(collection): release-1

---
+++ dist/assets/CollectionStore.2fd3b3fa.js
@@ -0,0 +1,1 @@
+import{d as n,a as r,c as s}from"./index.33ed05e9.js";const a=n("collection",()=>{const o=r({}),e=s(()=>o.value);function t(c){o.value=c}return{collection:o,get:e,store:t}});export{a as u};
